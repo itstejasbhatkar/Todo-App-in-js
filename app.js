c28abf6c0a7dde65c05ea13aa9bdc191711e47b6
Tejas Bhatkar <tejasbhatkar1439@gmail.com>
2024-08-09 15:59:47 +0530

    add new files

--- app.js
+++ app.js
@@ -96,6 +96,35 @@ document.getElementById('newTask').addEventListener('click', function (e) {
 });
 
 const blast = () =>{
+
+  const end = Date.now() + 15 * 1000;
+
+// go Buckeyes!
+const colors = ["#bb0000", "#ffffff"];
+
+(function frame() {
+  confetti({
+    particleCount: 2,
+    angle: 60,
+    spread: 55,
+    origin: { x: 0 },
+    colors: colors,
+  });
+
+  confetti({
+    particleCount: 2,
+    angle: 120,
+    spread: 55,
+    origin: { x: 1 },
+    colors: colors,
+  });
+
+  if (Date.now() < end) {
+    requestAnimationFrame(frame);
+  }
+})();
+
+
     const count = 200,
   defaults = {
     origin: { y: 0.7 },
